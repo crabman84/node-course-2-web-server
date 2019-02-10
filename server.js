de89a903817e13165618c05039c0f035fc3ab5a7
DreamCreateDreamAgain <lukegavin.5@outlook.com>
2019-02-10 18:53:58 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.reg
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, resp, next) =>{
+  resp.render('maintenance.hbs', {
+    pageTitle: 'A seal has blocked you -arf! arf!- ',
+    maintenanceMessage: 'This site is under maintenance! Come back later!'
+  })
+});
+// the following line must be after the
+//maintenance page else you can still access it
+app.use(express.static(__dirname + `/public`));
+
+app.use((req, resp, next) =>{
+  //We could do anything wel like Here
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}` ;
+
+  console.log(log);
+  fs.appendFile(`server.log`, log + "\n", (err) =>{
+    if(err){
+      console.log(`Unableto append to server.log`);
+    }
+  });
+  next();
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+
+app.get('/', (request, response) => {
+  // response.send('<h1>Hello Express!</h1>');
+  // response.send();
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    name: 'Luke',
+    welcomeMessage: 'Welcome sir!',
+    likes: [
+      `Biking`,
+      `Cities`
+    ]
+  })
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'ERROR: can not handle this request'
+  });
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
